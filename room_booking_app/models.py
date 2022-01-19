2a5ed203be41e7ea919e09ce982ac8870f3949a1
Maksym Gromiec <maxym1995@wp.pl>
2022-01-19 22:11:11 +0100

    Models

--- room_booking_app/models.py
+++ room_booking_app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Room(models.Model):
+    name = models.CharField(max_length=255)
+    capacity = models.PositiveIntegerField()
+    projector_aval = models.BooleanField(default=False)
